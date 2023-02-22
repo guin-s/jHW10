a9699f40dd9c6d2c29bf66adb1445de202ba9147
Viacheslav Kolarzh <guin-s@yandex.ru>
2023-02-22 03:16:11 +0300

    Extra tests for LastFilms added

--- src/test/java/FilmManagerTest.java
+++ src/test/java/FilmManagerTest.java
@@ -39,5 +39,192 @@ public class FilmManagerTest {
 
     }
 
+
+    @Test
+    public void lastFilmsTestWithBasicLimit() {
+        String film1 = "MovI";
+        String film2 = "MovII";
+        String film3 = "MovIII";
+        String film4 = "MovIV";
+        String film5 = "MovV";
+        String film6 = "MovVI";
+        String film7 = "MovVII";
+        String film8 = "MovVII";
+        String film9 = "MovIX";
+        String film10 = "MovX";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+
+        String[] actual = manager.findLast();
+        String[] expected = {film10, film9, film8, film7, film6, film5, film4, film3, film2, film1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+
+    @Test
+    public void lastFilmsTestWithGroupBiggerLimit() {
+        String film1 = "MovI";
+        String film2 = "MovII";
+        String film3 = "MovIII";
+        String film4 = "MovIV";
+        String film5 = "MovV";
+        String film6 = "MovVI";
+        String film7 = "MovVII";
+        String film8 = "MovVII";
+        String film9 = "MovIX";
+        String film10 = "MovX";
+        String film11 = "MovXI";
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+        String[] actual = manager.findLast();
+        String[] expected = {film11, film10, film9, film8, film7, film6, film5, film4, film3, film2};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void lastFilmsTestWithGroupSmallerThanLimit() {
+        String film1 = "MovI";
+        String film2 = "MovII";
+
+
+        FilmManager manager = new FilmManager();
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+
+
+        String[] actual = manager.findLast();
+        String[] expected = { film2, film1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void lastFilmsTestWithGroupBiggerCustomLimit() {
+        String film1 = "MovI";
+        String film2 = "MovII";
+        String film3 = "MovIII";
+        String film4 = "MovIV";
+        String film5 = "MovV";
+        String film6 = "MovVI";
+        String film7 = "MovVII";
+        String film8 = "MovVII";
+        String film9 = "MovIX";
+        String film10 = "MovX";
+        String film11 = "MovXI";
+
+        FilmManager manager = new FilmManager(5);
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        manager.addFilm(film5);
+        manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+        String[] actual = manager.findLast();
+        String[] expected = {film11, film10, film9, film8, film7};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void lastFilmsTestWithGroupEqualCustomLimit() {
+        //String film1 = "MovI";
+        //String film2 = "MovII";
+        //String film3 = "MovIII";
+        //String film4 = "MovIV";
+        //String film5 = "MovV";
+        //String film6 = "MovVI";
+        String film7 = "MovVII";
+        String film8 = "MovVII";
+        String film9 = "MovIX";
+        String film10 = "MovX";
+        String film11 = "MovXI";
+
+        FilmManager manager = new FilmManager(5);
+        //manager.addFilm(film1);
+        //manager.addFilm(film2);
+        //manager.addFilm(film3);
+        //manager.addFilm(film4);
+        //manager.addFilm(film5);
+        //manager.addFilm(film6);
+        manager.addFilm(film7);
+        manager.addFilm(film8);
+        manager.addFilm(film9);
+        manager.addFilm(film10);
+        manager.addFilm(film11);
+
+        String[] actual = manager.findLast();
+        String[] expected = {film11, film10, film9, film8, film7};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void lastFilmsTestWithGroupLessCustomLimit() {
+        String film1 = "MovI";
+        String film2 = "MovII";
+        String film3 = "MovIII";
+        String film4 = "MovIV";
+        //String film5 = "MovV";
+        //String film6 = "MovVI";
+        //String film7 = "MovVII";
+        //String film8 = "MovVII";
+        //String film9 = "MovIX";
+        //String film10 = "MovX";
+        //String film11 = "MovXI";
+
+        FilmManager manager = new FilmManager(5);
+        manager.addFilm(film1);
+        manager.addFilm(film2);
+        manager.addFilm(film3);
+        manager.addFilm(film4);
+        //manager.addFilm(film5);
+        //manager.addFilm(film6);
+        //manager.addFilm(film7);
+        //manager.addFilm(film8);
+        //manager.addFilm(film9);
+        //manager.addFilm(film10);
+        //manager.addFilm(film11);
+
+        String[] actual = manager.findLast();
+        String[] expected = {film4, film3, film2, film1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
 }
 
